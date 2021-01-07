6d49c3ff60d2191a7c4b97ac0a75328b900c796f
hamzaameer470 <61430169+hamzaameer470@users.noreply.github.com>
2021-01-07 17:19:30 +0500

    Add7

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -9,7 +9,7 @@ function AboutPage(props) {
             <Hero title={props.title} />
 
             <Content>
-            <p>Hello, my name is Garrett. I'm a full stack engineer with experience in Angular, AngularJS, Express JS, Node JS, Oracle SQL, MongoDB, and React.</p>
+            <p>Hello, my name is Hamza. I'm a full stack engineer with experience in Angular, AngularJS, Express JS, Node JS, Oracle SQL, MongoDB, and React.</p>
 
             <p>My dream is to one day start my own business and become an entrepreneur (currently working on some ideas).</p>
 
