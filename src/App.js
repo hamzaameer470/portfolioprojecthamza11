84e847889110f8effb484517d7b4ecf60baa358d
hamzaameer470 <61430169+hamzaameer470@users.noreply.github.com>
2021-01-07 17:06:20 +0500

    add3

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: 'Ameer Hamza',
+      title: 'Ameer Hamza ',
       headerLinks: [
         { title: 'Home', path: '/' },
         { title: 'About', path: '/about' },
